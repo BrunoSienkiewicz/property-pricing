d7b9715864db11ff4699d4ae60ec8ec59765f065
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2023-05-01 14:48:55 +0200

    Added models

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from database import Database, Listing
 from sql_queries import custom_sql_get
-from models import load_data_from_db_to_df, ohe_feature
+from models import load_data_from_db_to_df, ohe_feature, ohe_list_feature, build_model_random_forest
 import mysql.connector
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 def main():
     # database = Database("localhost", "root", "mysql", "test", "warszawa", 2, 4)
@@ -10,9 +13,26 @@ def main():
 
     # database.insert_to_db()
 
-    df = load_data_from_db_to_df(50, 'localhost', 'root', 'mysql')
+    # df = load_data_from_db_to_df(50, 'localhost', 'root', 'mysql')
+    # pass
+    # df.to_csv('data.csv')
+    df = pd.read_csv('data.csv')
+    df = df.drop(['Unnamed: 0'], axis=1)
+
+    features_to_ohe = ['City', 'Region', 'type of estate', 'MarketType', 'Building_type', 'Building_ownership', 'Building_material', 'Construction_status']
+    df = df.drop(['title', 'Estate_url', 'Date_added'], axis=1)
+    feature_list_to_ohe = ['Equipment_types', 'Extras_types', 'Media_types', 'Security_types']
+
+    for feature in features_to_ohe:
+        df = ohe_feature(df, feature)
+
+    for feature in feature_list_to_ohe:
+        df = ohe_list_feature(df, feature)
+    
+    print(df.columns)
+    model, mse = build_model_random_forest(df, 0.2, df.columns, 'Total Price')
+    print(mse)
     pass
-    df.to_csv('data.csv')
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import mysql.connector
 from sql_queries import get_all_property_info
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, MultiLabelBinarizer
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
@@ -28,6 +29,11 @@ def build_model_sk(data, model, train_test_ratio, chosen_features, target, rando
     MSE_test = mean_squared_error(y_test, y_pred_test)
     
     return model, [MSE_test, MSE_train]
+
+
+def build_model_random_forest(data, train_test_ratio, chosen_features, target, random_state=45):
+    model = RandomForestRegressor(random_state=random_state)
+    return build_model_sk(data, model, train_test_ratio, chosen_features, target, random_state)
 
 
 def load_data_from_db_to_df(amt, host, user, password):
@@ -53,5 +59,14 @@ def load_data_from_db_to_df(amt, host, user, password):
 def ohe_feature(df, feature_name):
     encoder = OneHotEncoder()
     encoded_feature = encoder.fit_transform(df[[feature_name]])
-    df_encoded = pd.concat([df.drop([feature_name], axis=1), pd.DataFrame(encoded_feature.toarray(), columns=encoder.get_feature_names([feature_name]))], axis=1)
+    df_encoded = pd.concat([df.drop([feature_name], axis=1), pd.DataFrame(encoded_feature.toarray(), columns=encoder.get_feature_names_out([feature_name]))], axis=1)
+    return df_encoded
+
+
+def ohe_list_feature(df, feature):
+    mlb = MultiLabelBinarizer()
+    df[feature] = df[feature].apply(lambda x: 'none_' + feature if pd.isna(x) else x)
+    df[feature] = df[feature].apply(lambda x: x.split(','))
+    encoded_feature = mlb.fit_transform(df[feature])
+    df_encoded = pd.concat([df.drop([feature], axis=1), pd.DataFrame(encoded_feature, columns=mlb.classes_)], axis=1)
     return df_encoded
