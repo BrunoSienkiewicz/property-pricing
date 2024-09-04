85483bd8ca29f79aab5f4efcea33f4a4ad2cd806
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-09-04 18:27:21 +0200

    update: model config

--- model/utils.py
+++ model/utils.py
@@ -5,6 +5,7 @@ import joblib
 import pandas as pd
 import torch
 import torch.nn as nn
+from pathlib import Path
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 
 
@@ -27,24 +28,32 @@ def save_model(
     ohe_encoder: OneHotEncoder = None,
     ordinal_encoder: OrdinalEncoder = None,
     scaler: StandardScaler = None,
+    model_directory: os.PathLike = Path(os.curdir) / "artifacts",
 ):
+    if not os.path.exists(model_directory):
+        os.makedirs(model_directory, exist_ok=True)
+    model_path = model_directory / f"{config['model_name']}_{config['model_version']}"
+
     torch.save(
-        model.state_dict(), f"{config['model_name']}_{config['model_version']}.pth"
+        model.state_dict(), model_path / "model.pth"
     )
-    with open(f"{config['model_name']}_{config['model_version']}.json", "w") as file:
+    with open(model_path / "model.pth", "rb") as file:
         binary = file.read()
+
     if ohe_encoder:
         ohe_encoder_path = (
-            f"{config['model_name']}_{config['model_version']}_ohe_encoder.pkl"
+            model_path / "ohe_encoder.pkl"
         )
         joblib.dump(ohe_encoder, ohe_encoder_path)
     if ordinal_encoder:
         ordinal_encoder_path = (
-            f"{config['model_name']}_{config['model_version']}_ordinal_encoder.pkl"
+            model_path / "ordinal_encoder.pkl"
         )
         joblib.dump(ordinal_encoder, ordinal_encoder_path)
     if scaler:
-        scaler_path = f"{config['model_name']}_{config['model_version']}_scaler.pkl"
+        scaler_path = (
+            model_path / "scaler.pkl"
+        )
         joblib.dump(scaler, scaler_path)
 
     df = pd.DataFrame(
@@ -54,9 +63,9 @@ def save_model(
             "model_description": [config["model_description"]],
             "model_metadata": [json.dumps(config["model_metadata"])],
             "model_binary": [binary],
-            "model_ohe_encoder": [ohe_encoder],
-            "model_ordinal_encoder": [ordinal_encoder],
-            "model_scaler": [scaler],
+            "model_ohe_encoder": [joblib.load(ohe_encoder_path)],
+            "model_ordinal_encoder": [joblib.load(ordinal_encoder_path)],
+            "model_scaler": [joblib.load(scaler_path)],
         }
     )
     df.to_sql(
@@ -66,3 +75,35 @@ def save_model(
         index=False,
         schema="model_store",
     )
+
+
+def load_model_from_db(
+    model_name: str,
+    model_version: str,
+    connection_string: str,
+):
+    df = pd.read_sql(
+        f"SELECT * FROM model_store.models WHERE model_name='{model_name}' AND model_version='{model_version}'",
+        connection_string,
+    )
+
+    model_state_dict = df["model_binary"].values[0]
+    metadata = json.loads(df["model_metadata"].values[0])
+    ohe_encoder = joblib.load(df["model_ohe_encoder"].values[0])
+    ordinal_encoder = joblib.load(df["model_ordinal_encoder"].values[0])
+    scaler = joblib.load(df["model_scaler"].values[0])
+
+    return model_state_dict, metadata, ohe_encoder, ordinal_encoder, scaler
+
+def load_model_from_directory(
+    model_name: str,
+    model_version: str,
+    model_directory: os.PathLike,
+):
+    model_path = model_directory / f"{model_name}_{model_version}"
+    model_state_dict = torch.load(model_path / "model.pth")
+    ohe_encoder = joblib.load(model_path / "ohe_encoder.pkl")
+    ordinal_encoder = joblib.load(model_path / "ordinal_encoder.pkl")
+    scaler = joblib.load(model_path / "scaler.pkl")
+
+    return model_state_dict, ohe_encoder, ordinal_encoder, scaler
